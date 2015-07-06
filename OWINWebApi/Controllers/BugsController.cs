1c0b08dbf1fd2744e85d66e391c890054cf6158c
BraveShine <lzlzlz911@hotmail.com>
2015-07-06 18:02:31 +0800

    asd

---
+++ OWINWebApi/Controllers/BugsController.cs
@@ -0,0 +1,13 @@
+﻿namespace OWINWebApi.Controllers{
+
+    using System.Web.Http;
+
+    using Newtonsoft.Json;
+
+    public class BugsController : ApiController{
+
+        public string Get(string id) { return JsonConvert.SerializeObject(new{Name = "saber"}); }
+
+    }
+
+}
---
+++ OWINWebApi/Startup1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(OWINWebApi.Startup1))]
+
+namespace OWINWebApi
+{
+
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("bugs", "api/{Controller}/{id}", new{id = RouteParameter.Optional});
+            app.UseWebApi(config);
+        }
+    }
+}
